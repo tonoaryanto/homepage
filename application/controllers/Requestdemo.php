7051e87f532c8e0f61bdf7056bc9f97d55f526ee
tonoaryanto <tonoaryanto@gmail.com>
2021-03-22 09:25:35 +0700

    Verifikasi add strlower2

--- application/controllers/Requestdemo.php
+++ application/controllers/Requestdemo.php
@@ -21,7 +21,7 @@ class Requestdemo extends CI_Controller {
         if($this->cek_karakter('text',strtolower($nama)) == 1){$status = 1; $input = "name";}
 		if($this->cek_karakter('text',strtolower($company)) == 1 or $status == 1){$status = 1; $input = "company";}
 		if($this->cek_karakter('email',strtolower($email)) == 1 or $status == 1){$status = 1; $input = "email";}
-		if($this->cek_karakter('number',$code) == 1 or $status == 1){$status = 1; $input = "code phone";}
+		if($this->cek_karakter('number',($code) == 1 or $status == 1){$status = 1; $input = "code phone";}
 		if($this->cek_karakter('number',$phone) == 1 or $status == 1){$status = 1; $input = "number phone";}
 
 		if($status == 1){
@@ -80,9 +80,9 @@ class Requestdemo extends CI_Controller {
 
         //Validasi
          $input = "";
-         if($this->cek_karakter('number',$rawphone) == 1 or $rawphone == ""){$status = 1; $input = "number phone";}
-         if($this->cek_karakter('number',$code) == 1 or $code == ""){$status = 1; $input = "code phone";}
-         if($this->cek_karakter('email',$email) == 1 or $email == "" or $status == 1){$status = 1; $input = "email";}
+         if($this->cek_karakter('number',strtolower($rawphone)) == 1 or $rawphone == ""){$status = 1; $input = "number phone";}
+         if($this->cek_karakter('number',strtolower($code)) == 1 or $code == ""){$status = 1; $input = "code phone";}
+         if($this->cek_karakter('email',strtolower($email)) == 1 or $email == "" or $status == 1){$status = 1; $input = "email";}
          if($this->cek_karakter('text',$company) == 1 or $company == "" or $status == 1){$status = 1; $input = "company";}
          if($this->cek_karakter('text',$nama) == 1 or $nama == "" or $status == 1){$status = 1; $input = "name";}
  
