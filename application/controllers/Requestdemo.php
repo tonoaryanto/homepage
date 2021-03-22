8421ac9f854d466df3c88fe932d81ed9ad9c000c
tonoaryanto <tonoaryanto@gmail.com>
2021-03-22 09:30:09 +0700

    Rev Error verif

--- application/controllers/Requestdemo.php
+++ application/controllers/Requestdemo.php
@@ -21,7 +21,7 @@ class Requestdemo extends CI_Controller {
         if($this->cek_karakter('text',strtolower($nama)) == 1){$status = 1; $input = "name";}
 		if($this->cek_karakter('text',strtolower($company)) == 1 or $status == 1){$status = 1; $input = "company";}
 		if($this->cek_karakter('email',strtolower($email)) == 1 or $status == 1){$status = 1; $input = "email";}
-		if($this->cek_karakter('number',($code) == 1 or $status == 1){$status = 1; $input = "code phone";}
+		if($this->cek_karakter('number',$code) == 1 or $status == 1){$status = 1; $input = "code phone";}
 		if($this->cek_karakter('number',$phone) == 1 or $status == 1){$status = 1; $input = "number phone";}
 
 		if($status == 1){
